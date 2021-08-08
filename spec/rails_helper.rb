125c85a38e0948530c7397d231c7c945b929a947
Richard <rdesilvey@gmail.com>
2021-08-08 14:05:05 -0600

    Fix keys for vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,8 +77,8 @@ VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.filter_sensitive_data("<key>") { ENV['OPEN_WEATHER_API_KEY'] }
-  config.filter_sensitive_data("<appid>") { ENV['GEO_CODING_API_KEY'] }
+  config.filter_sensitive_data("<key>") { ENV['GEO_CODING_API_KEY'] }
+  config.filter_sensitive_data("<appid>") { ENV['OPEN_WEATHER_API_KEY'] }
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
 end
