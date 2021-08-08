bf4eacd0dd5b327d28c96fdcf7a908500f708f7c
Richard <rdesilvey@gmail.com>
2021-08-08 13:48:07 -0600

    Configure route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      resource :forcast, only: :show
+
       namespace :users do
       end
-      
+
       namespace :sessions do
       end
     end
