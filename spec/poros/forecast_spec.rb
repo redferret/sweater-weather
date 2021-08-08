4e102465b754457facf5489a3e066c69549553f7
Richard <rdesilvey@gmail.com>
2021-08-08 13:05:55 -0600

    Write poro test

---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Forecast do
+  describe 'instance,' do
+    before :each do
+      attrs = {
+        current_weather: {},
+        daily_weather: {},
+        hourly_weather: {}
+      }
+      @forecast = Forecast.new(attrs)
+    end
+
+    it 'exists' do
+      expect(@forecast).to be_a Forecast
+    end
+
+    it 'has expected attributes' do
+      expect(@forecast.current_weather).to be_a Hash
+      expect(@forecast.daily_weather).to be_a Hash
+      expect(@forecast.hourly_weather).to be_a Hash
+    end
+  end
+
+  describe 'class method,' do
+    context '::model_name' do
+      it 'returns the name of the model' do
+        expect(Forecast.model_name).to eq 'Forecast'
+      end
+    end
+  end
+end
