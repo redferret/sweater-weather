2e153a3b958167c326354387524099e0a5378c66
Richard <rdesilvey@gmail.com>
2021-08-10 11:38:44 -0600

    Implement poro

---
+++ app/poros/background.rb
@@ -0,0 +1,12 @@
+class Background
+  alias :read_attribute_for_serialization :send
+  attr_reader :image
+
+  def initialize(attrs)
+    @image = attrs[:image]
+  end
+
+  def self.model_name
+    @_model_name ||= ActiveModel::Name.new(self)
+  end
+end
