0e6c3fd88c6e1b666240d2ce35a9567090003298
Richard <rdesilvey@gmail.com>
2021-08-10 09:15:52 -0600

    Write service test

---
+++ spec/services/open_library/api/search_books_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe OpenLibrary::API::ApiEndpoints, :vcr do
+  VCR.use_cassette('search_books', record: :new_episodes) do
+    describe 'search_books' do
+      it 'returns expected data when successful' do
+        query = 'denver,co'
+        response = OpenLibrary::API::ApiEndpoints.search_books(query, 5)
+
+        expect(response).to have_key(:docs)
+
+        doc = response[:docs].first
+        
+        expect(doc).to have_key(:isbn)
+        expect(doc).to have_key(:publisher)
+        expect(doc).to have_key(:title)
+      end
+    end
+  end
+end
