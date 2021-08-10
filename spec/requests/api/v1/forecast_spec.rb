abca6d4c6b0222d0b662b1a67c0098173af5b90e
Richard <rdesilvey@gmail.com>
2021-08-10 10:10:29 -0600

    Use other cassettes

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -1,42 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe 'Forecast Request' do
-  describe 'GET /api/v1/forecast' do
-    context 'when valid query param given' do
-      before :each do
-        get '/api/v1/forecast?location=denver,co'
-      end
+RSpec.describe 'Forecast Request', :vcr do
+  VCR.use_cassette('onecall', record: :new_episodes) do
+    VCR.use_cassette('geo_address', record: :new_episodes) do
+      describe 'GET /api/v1/forecast' do
+        context 'when valid query param given' do
+          before :each do
+            get '/api/v1/forecast?location=denver,co'
+          end
 
-      it 'returns with status 200' do
-        expect(response).to have_http_status 200
-      end
+          it 'returns with status 200' do
+            expect(response).to have_http_status 200
+          end
 
-      it 'request body has expected json format' do
-        json = Oj.load(response.body, symbol_keys: true)
+          it 'request body has expected json format' do
+            json = Oj.load(response.body, symbol_keys: true)
 
-        expect(json).to have_key(:data)
-        data = json[:data]
+            expect(json).to have_key(:data)
+            data = json[:data]
 
-        expect(data).to have_key(:id)
-        expect(data).to have_key(:type)
-        expect(data[:type]).to eq('forecast')
-        expect(data).to have_key(:attributes)
+            expect(data).to have_key(:id)
+            expect(data).to have_key(:type)
+            expect(data[:type]).to eq('forecast')
+            expect(data).to have_key(:attributes)
 
-        attributes = data[:attributes]
+            attributes = data[:attributes]
 
-        expect(attributes).to have_key(:current_weather)
-        expect(attributes).to have_key(:daily_weather)
-        expect(attributes).to have_key(:hourly_weather)
-      end
-    end
+            expect(attributes).to have_key(:current_weather)
+            expect(attributes).to have_key(:daily_weather)
+            expect(attributes).to have_key(:hourly_weather)
+          end
+        end
 
-    context 'when empty or missing query param is given' do
-      it 'returns status 404' do
-        get '/api/v1/forecast'
-        expect(response).to have_http_status 404
+        context 'when empty or missing query param is given' do
+          it 'returns status 404' do
+            get '/api/v1/forecast'
+            expect(response).to have_http_status 404
 
-        get '/api/v1/forecast?location'
-        expect(response).to have_http_status 404
+            get '/api/v1/forecast?location'
+            expect(response).to have_http_status 404
+          end
+        end
       end
     end
   end
--- spec/services/open_library/api/search_books_spec.rb
+++ spec/services/open_library/api/search_books_spec.rb
@@ -8,9 +8,10 @@ RSpec.describe OpenLibrary::API::ApiEndpoints, :vcr do
         response = OpenLibrary::API::ApiEndpoints.search_books(query, 5)
 
         expect(response).to have_key(:docs)
+        expect(response).to have_key(:numFound)
 
         doc = response[:docs].first
-        
+
         expect(doc).to have_key(:isbn)
         expect(doc).to have_key(:publisher)
         expect(doc).to have_key(:title)
